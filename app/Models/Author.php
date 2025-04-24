1a71b89f3f3c5dc04b3916eafb46a74bc058a3a0
kylianLaw <iladelek@gmail.com>
2025-04-24 16:44:14 -0400

    done with step 2

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Author extends Model
 {
-    //
+    use HasFactory;
+
+    public function posts()
+    {
+        return $this-> hasMany(Post::class);
+    }
+
+    public function scopeName(Builder $query, string $name): Builder
+    {
+        return $query->where('name', 'LIKE', '%' . $name . '%');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function scopeCommenterName(Builder $query, string $name): Builder
+    {
+        return $query->where('commenter_name', 'LIKE', '%' . $name . '%');
+    }
 }
